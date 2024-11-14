0651e978c8c13c376161a1808c83348d3e07dba7
Albane <albane.dorin@gmail.com>
2024-11-14 03:14:36 +0100

    Front end of the side bar

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,8 +6,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return flask.render_template('home.html.jinja2')
 
+@app.route('/connexion')
+def connexion():
+    return flask.render_template('connexion.html.jinja2')
+
+@app.route('/inscription')
+def inscription():
+    return flask.render_template('inscription.html.jinja2')
 
 if __name__ == '__main__':
     app.run()
